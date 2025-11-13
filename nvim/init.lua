66bc8b939e180138e44bdb130b8e9be0fc97372d
Egor Averyanov <egor.a2004@gmail.com>
2025-11-13 11:49:43 +0300

    vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -84,7 +84,7 @@ vim.keymap.set('n', '<Leader>ls', ':Telescope lsp_dynamic_workspace_symbols<cr>'
 
 -- Color
 vim.cmd.set('background=dark')
-vim.cmd.colorscheme('evening')
+vim.cmd.colorscheme('vim')
 
 -- Vsnip
 vim.cmd("imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)'      : '<Tab>'")
